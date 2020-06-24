5237435c795fdfa29c50e704340afa46d91ce37e
ariela <ariela.haley65@gmail.com>
2020-06-23 20:38:26 -0700

    :building_construction:

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@
 git init                        #Turn an existing directory into a git repo
 git clone [url]                 #Clone a repo that already exists on Github
 .gitgnore                       #Special file that ignores commits for directories and files named in it 
-
+git status                      #Shows which files are in the staging area 
 ######################
 #Configure Tooling
 ######################
@@ -19,8 +19,12 @@ git config --global color.ui auto   #Enables helpful colorization of command lin
 ######################
 git branch [branch-name]       #Creates a new branch
 git checkout [branch-name]     #Switches to the specified branch and updates the working directory
+git checkout -b [branch-name]  #Creates and switches to a branch 
 git merge [branch]             #Combines the specified branch’s history into the current branch. This is usually done in pull requests,but is an important Git operation.
 git branch -d [branch-name]    #Deletes the specified branch
+git branch -D [branch-name]    #Force Delete even if branch is unmerged 
+git branch -a                  #Checks branches 
+git branch -m [branch-name]    #Renames branch 
 
 ######################
 #Synchronize changes 
@@ -28,16 +32,19 @@ git branch -d [branch-name]    #Deletes the specified branch
 git fetch                     #Downloads all history from the remote tracking branches
 git merge                     #Combines remote tracking branch into current local branch
 git push                      #Uploads all local branch commits to GitHub
-git push origin master
+git push origin master        #Uploads to master branch 
+igt push origin [branch-name] #Push changes to branch 
 git pull                      #Updates your current local working branch with all new commits from the corresponding remote branch on GitHub. git pull is a combination of git fetch and git merge 
 ######################
 #Make Changes 
 ######################
 git log                       #Lists version history for the current branch
+git log --oneline             #Lists version history in one line 
 git log --follow [file]       #Lists version history for a file, including renames
 git diff [first-branch]...[second-branch] #Shows content differences between two branches
 git show [commit]            #Outputs metadata and content changes of the specified commit
 git add [file]               #Snapshots the file in preparation for versioning
+git rm --cached [file]       #Unstages any file in the staging area 
 git commit -m "[descriptive message]" #Records file snapshots permanently in version history
 
 ######################
@@ -62,3 +69,20 @@ git remote -v   #Verify the remote connections
 ######################
 git commit && git push
 git commit || echo "Commit failed"
+
+
+######################
+#Contributing to Open Source  
+######################
+1. Fork Repo that you want to contriubte to 
+2. git clone [repo url]
+3. git status   #Check the repo is up to date with the master branch 
+4. Create a new branch with:
+   - git checkout -b [branch-name]
+5. Make changes to the repo 
+6. Push changes to branch:
+     - git add .
+     - git commit -m "contributing"
+     - git push origin [branch-name]
+7. Comapre and Pull request from github 
+8. Create a new pull request on github 
