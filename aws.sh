9e876a6d8fef5433bfa38ae6d0a1919223c6c989
ariela <ariela.haley65@gmail.com>
2020-07-25 03:45:35 -0700

    :building_construction:

---
+++ aws.sh
@@ -0,0 +1,14 @@
+#AWS CLI commands 
+
+aws config      #sets up aws cli installation 
+......
+AWS Access Key ID [None]: <access key>
+AWS Secret Access Key [None]: <secret key>
+Default region name [None]: us-west-2
+Default output format [None]: json
+
+aws configure list     #list all configuration data
+
+aws configure list-profiles  #list all profile names
+
+
