5e1b131bf3646a3f8427eb87949a69dc184be7a0
Ari <ariela.haley65@gmail.com>
2020-12-02 23:38:32 -0800

    adding cobra setup

---
+++ spf13-cobra.sh
@@ -0,0 +1,10 @@
+######################
+#Install golang, cobra, and setup a project 
+######################
+1. Install golang: > brew install go | or check out official install https://golang.org/doc/install#install
+2. Create a new project and cd into it
+3. Install cobra library: > go get -u github.com/spf13/cobra/cobra
+4. Create a project: > cobra init --pkg-name <project-name>
+5. Install go modules: > go mod init <project-name>
+6. Build: > go install <project-name>
+7. Now you can run <project-name> to use the Cli
