ddbb49f693c085711070fafae8966364234c839e
Ari <36749814+ari-hacks@users.noreply.github.com>
2021-03-15 21:14:23 -0700

    Update spf13-cobra.sh

--- spf13-cobra.sh
+++ spf13-cobra.sh
@@ -6,6 +6,8 @@
 3. Install cobra library: > go get -u github.com/spf13/cobra/cobra
 4. Create a project: > cobra init --pkg-name <project-name>
 5. Install go modules: > go mod init <project-name>
-6. Build: > go install <project-name>
+6. Install & Build: 
+   > go install <project-name>
+   > go build <project-name>
 7. Now you can run <project-name> to use the Cli
 8. Add commands: > cobra add <command-name>
